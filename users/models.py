c213401eadd1e6e76ec1990cee454e3c454f89b6
Artem Komlev <komlev.artem.02@mail.ru>
2024-11-09 14:28:37 +0700

    Добавлена модель пользователя с ролями 'администратор' и 'клиент'.

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Администратор'),
+        ('client', 'Клиент'),
+    ]
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='client')
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
+    
+# Create your models here.
